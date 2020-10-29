5126051bba291d1d737a795c2817d157a4c08956
VladinasM <69033234+VladinasM@users.noreply.github.com>
2020-10-29 06:59:36 +0300

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,19 @@
+import random
+list1 = list()
+for i in range(30):
+   list1.append(random.randint(-60, 60))
+print(list1)
+
+minElem = min(list1)
+maxElem = max(list1)
+print('Минимальное число: ', minElem)
+ln = len(list1)
+for i in range(len(list1)):
+   a = int(list1[i])
+   if list1.index(minElem) < list1.index(a) and a > 0 and list1.index(a)%2==0:
+      list1.remove(a)
+      list1.insert(i, ' ')
+while ' ' in list1:
+   list1.remove(' ')
+
+print(list1)
